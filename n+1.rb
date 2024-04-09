ae114a834dd7882055658eba6ec4f6c8df8729d3
kunal-supe <supe.s.kunal@gmail.com>
2024-04-10 07:58:39 +1000

    n+1 notes

---
+++ n+1.rb
@@ -0,0 +1,42 @@
+Example:
+
+#Model
+#Blog --> belongs to student
+#Student --> has many blogs
+
+#Get all blogs
+blogs = Blogs.all
+blogs.each do |blog|
+  blog.student.email
+end
+
+
+# Eager Loading
+
+@blogs = Blog.includes(:student)
+
+# Russian doll caching
+<% @blogs.each do |blog| %>
+  <% cache blog do %>
+    <%= render blog %>
+  <% end %>
+<% end %>
+
+# Use strict_loading
+
+class Student < ApplicationRecord
+  has_many :blogs, strict_loading: true
+end
+
+# counter_cache
+
+class Student < ApplicationRecord  
+  has_many :blogs, counter_cache: true
+end
+
+# To do this, we can just add a new column to the student table named blogs_count.
+
+create_table :student do |t|
+  # [Rest of the columns]
+  t.integer :blogs_count, default: 0
+end
